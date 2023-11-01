102a1acc234fbc74660abfbf5b7a82bba9dc8da6
Maksim <homeice@mail.ru>
2023-11-01 05:29:29 +0300

    add seminar 5

---
+++ unit-tests-5/src/seminars/five/number/MaxNumberModule.java
@@ -0,0 +1,10 @@
+package seminars.five.number;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MaxNumberModule {
+    public int getMaxvalue(List<Integer> randomList) {
+        return Collections.max(randomList);
+    }
+}
